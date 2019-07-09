10b005f8e517f94fbc18022abc919693788970c7
Bret Walker <bw@bretw.com>
2019-07-09 09:44:30 -0400

    WIP

--- renters_rights/renters_rights/urls.py
+++ renters_rights/renters_rights/urls.py
@@ -9,4 +9,3 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
