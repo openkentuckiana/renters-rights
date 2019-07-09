58ed5ed9f9396b392da38e672c5e87ae625f22d0
Bret Walker <bw@bretw.com>
2019-07-09 10:22:52 -0400

    WIP

--- renters_rights/renters_rights/urls.py
+++ renters_rights/renters_rights/urls.py
@@ -9,3 +9,6 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    # Debug could be on when deployed, but we'd be using django-storages and a remote location for media storage
+    if settings.MEDIA_URL and settings.MEDIA_ROOT:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
