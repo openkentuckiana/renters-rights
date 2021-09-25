a1324a58eea1568637f5920042a89ac46568d8ce
Bret Walker <bretwalker@Faithful.local>
2021-09-25 14:15:24 -0400

    How it works

--- renters_rights/rules/urls.py
+++ renters_rights/rules/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import GetHelpView, IndexView, RulesView, RuleView
+from .views import (
+    GetHelpView,
+    HowItWorksView,
+    IndexView,
+    ResourcesView,
+    RulesView,
+    RuleView,
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="homepage"),
+    path("how-it-works", HowItWorksView.as_view(), name="how-it-works"),
     path("get-help", GetHelpView.as_view(), name="get-help"),
+    path("resources", ResourcesView.as_view(), name="resources"),
     path("rules", RulesView.as_view(), name="rules"),
     path("rules/<slug:slug>", RuleView.as_view(), name="rule"),
 ]
